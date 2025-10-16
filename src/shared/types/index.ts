388dc50ce189d31a368e2a9e57c491ee62825c08
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-16 15:09:47 -0700

    Create index.ts

---
+++ src/shared/types/index.ts
@@ -0,0 +1,10 @@
+// Export everything
+export * from './env.types';
+export * from './analysis.types';
+export * from './business.types';
+export * from './billing.types';
+export * from './user.types';
+export * from './analytics.types';
+export * from './bulk.types';
+export * from './api.types';
+export * from './legacy.types';
