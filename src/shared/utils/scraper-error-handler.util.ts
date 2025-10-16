71254f816515733944bde0b0a0f252ddd52beade
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-16 15:32:21 -0700

    Update scraper-error-handler.util.ts

--- src/shared/utils/scraper-error-handler.util.ts
+++ src/shared/utils/scraper-error-handler.util.ts
@@ -1,4 +1,4 @@
-import { logger } from './logger.js';
+import { logger } from './logger.util.js';
 
 export class ScraperErrorHandler {
   static transformError(error: any, username: string): Error {
